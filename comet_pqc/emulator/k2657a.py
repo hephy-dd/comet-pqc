32c05b65cd748304a829c57db1f1bbeb0f31d16a
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-06-08 16:42:10 +0200

    updated emulator

--- comet_pqc/emulator/k2657a.py
+++ comet_pqc/emulator/k2657a.py
@@ -38,18 +38,97 @@ class ErrorQueueMixin:
     def write_errorqueue_clear(self, message):
         type(self).count = 0
 
+class MeasureMixin:
+
+    filter_count = 0
+    filter_enable = False
+    filter_type = 0
+    nplc = 1.0
+
+    @message(r'print\(smua\.measure\.filter\.count\)')
+    def query_measure_filter_count(self, message):
+        return format(float(type(self).filter_count), 'E')
+
+    @message(r'smua\.measure\.filter\.count\s*\=\s*(\d+)')
+    def write_measure_filter_count(self, message):
+        type(self).filter_count = int(message.split('=')[-1])
+
+    @message(r'print\(smua\.measure\.filter\.enable\)')
+    def query_measure_filter_enable(self, message):
+        return format(float(type(self).filter_enable), 'E')
+
+    @message(r'smua\.measure\.filter\.enable\s*\=\s*(\d+)')
+    def write_measure_filter_enable(self, message):
+        type(self).filter_enable = bool(int(message.split('=')[-1]))
+
+    @message(r'print\(smua\.measure\.filter\.type\)')
+    def query_measure_filter_type(self, message):
+        return format(float(type(self).filter_type), 'E')
+
+    @message(r'smua\.measure\.filter\.type\s*\=\s*(.*)')
+    def write_measure_filter_type(self, message):
+        d = {'MOVING': 0, 'REPEAT': 1, 'MEDIAN': 2}
+        type(self).filter_type = d[message.split('=')[-1]]
+
+    @message(r'print\(smua\.measure\.nplc\)')
+    def query_measure_nplc(self, message):
+        return format(float(type(self).nplc), 'E')
+
+    @message(r'smua\.measure\.nplc\s*\=\s*(.*)')
+    def write_measure_nplc(self, message):
+        type(self).nplc = float(message.split('=')[-1])
+
+    @message(r'print\(smua\.measure\.i\(\)\)')
+    def query_measure_i(self, message):
+        return format(float(random.uniform(0.001, 0.01), 'E'))
+
+    @message(r'print\(smua\.measure\.v\(\)\)')
+    def query_measure_v(self, message):
+        return format(float(random.uniform(1.0, 10.0), 'E'))
+
+    @message(r'print\(smua\.measure\.r\(\)\)')
+    def query_measure_r(self, message):
+        return format(float(random.random(), 'E'))
+
+    @message(r'print\(smua\.measure\.p\(\)\)')
+    def query_measure_p(self, message):
+        return format(float(random.random(), 'E'))
+
+    @message(r'print\(smua\.measure\.iv\(\)\)')
+    def query_measure_iv(self, message):
+        return '{},{}'.format(random.uniform(0.001, 0.01), random.uniform(1.0, 10.0))
+
 class SourceMixin:
 
     compliance = False
+    func = 0
+    highc = False
     leveli = 0.0
     levelv = 0.0
     limiti = 0.0
     limitv = 0.0
-    output = False
+    output = 0
 
     @message(r'print\(smua\.source\.compliance\)')
     def query_source_compliance(self, message):
         return format(int(type(self).compliance), 'E')
+
+    @message(r'print\(smua\.source\.func\)')
+    def query_source_func(self, message):
+        return format(float(type(self).func), 'E')
+
+    @message(r'smua\.source\.func\s*\=\s*(.*)')
+    def write_source_func(self, message):
+        d = {'DCAMPS': 0, 'DCVOLTS': 1}
+        type(self).func = d[message.split('=')[-1]]
+
+    @message(r'print\(smua\.source\.highc\)')
+    def query_source_highc(self, message):
+        return format(float(type(self).highc), 'E')
+
+    @message(r'smua\.source\.highc\s*\=\s*(\d+)')
+    def write_source_highc(self, message):
+        type(self).highc = bool(int(message.split('=')[-1]))
 
     @message(r'print\(smua\.source\.leveli\)')
     def query_source_leveli(self, message):
@@ -89,11 +168,23 @@ class SourceMixin:
 
     @message(r'smua\.source\.output\s*\=\s*(\d+)')
     def write_source_outout(self, message):
-        type(self).output = bool(int(message.split('=')[-1]))
+        type(self).output = int(message.split('=')[-1])
 
-class K2657AHandler(IEC60488Handler, BeeperMixin, ErrorQueueMixin, SourceMixin):
+class K2657AHandler(IEC60488Handler, BeeperMixin, ErrorQueueMixin, MeasureMixin,
+                    SourceMixin):
 
     identification = "Spanish Inquisition Inc., Model 2657A, 12345678, v1.0"
+
+    smua_sense = 0
+
+    @message(r'print\(smua\.sense\)')
+    def query_smua_sense(self, message):
+        return format(float(type(self).smua_sense), 'E')
+
+    @message(r'smua\.sense\s*\=\s*(\d+)')
+    def write_smua_sense(self, message):
+        d = {'LOCAL': 0, 'REMOTE': 1, 'CALA': 2}
+        type(self).smua_sense = d[message.split('=')[-1]]
 
 if __name__ == "__main__":
     run(K2657AHandler)
