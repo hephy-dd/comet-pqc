277b14b7adea55fb2aa4401bd0b8586382672ddb
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-08-06 12:47:30 +0200

    exception handling, refs #12

--- comet_pqc/processes/resource.py
+++ comet_pqc/processes/resource.py
@@ -15,7 +15,7 @@ class ResourceRequest:
     def __init__(self, command):
         self.command = command
         self.result = None
-        #self.error = None
+        self.error = None
         self.ready = threading.Event()
 
     def get(self, timeout=10.0):
@@ -23,14 +23,15 @@ class ResourceRequest:
         while not self.ready.is_set():
             if t < time.time():
                 raise RuntimeError(f"Request timeout: {self.command}")
-        #if self.error is not None:
-        #    raise self.error
+        if self.error is not None:
+            raise self.error
         return self.result
 
     def dispatch(self, context):
         try:
             self.result = self.command(context)
-        except:
+        except Exception as exc:
+            self.error = exc
             raise
         finally:
             self.ready.set()
@@ -45,6 +46,7 @@ class ResourceProcess(Process, ResourceMixin):
         super().__init__(**kwargs)
         self.name = name
         self.enabled = enabled
+        self.__failed_retries = 0
         self.__queue = queue.Queue()
         self.__lock = threading.RLock()
         self.__context_lock = threading.RLock()
@@ -67,30 +69,36 @@ class ResourceProcess(Process, ResourceMixin):
 
     def request(self, callback):
         with self.__lock:
+            if not self.enabled:
+                raise RuntimeError("service not enabled")
             r = ResourceRequest(callback)
             self.__queue.put(r)
             return r.get()
 
     def serve(self):
-        with self.resources.get(self.name) as resource:
-            driver = type(self).Driver(resource)
-            while True:
-                if not self.running:
-                    break
-                if not self.enabled:
-                    break
-                if not self.__queue.empty():
-                    r = self.__queue.get()
-                    r.dispatch(driver)
-                time.sleep(self.throttle_time)
+        logging.info("start serving %s", self.name)
+        try:
+            with self.resources.get(self.name) as resource:
+                driver = type(self).Driver(resource)
+                while True:
+                    if not self.running:
+                        break
+                    if not self.enabled:
+                        break
+                    if not self.__queue.empty():
+                        r = self.__queue.get()
+                        r.dispatch(driver)
+                    time.sleep(self.throttle_time)
+        finally:
+            logging.info("stopped serving %s", self.name)
 
     def run(self):
         while self.running:
             if self.enabled:
-                self.serve()
                 try:
                     self.serve()
                 except Exception as exc:
                     logging.error("%s: %s", type(self).__name__, exc)
                     tb = traceback.format_exc()
                     self.emit('failed', exc, tb)
+            time.sleep(self.throttle_time)
