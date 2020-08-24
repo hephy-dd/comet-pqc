72b72f99f807b34c7f0d9f229530e242b3c307ac
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-08-24 12:47:27 +0200

    version 0.22

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='comet-pqc',
-    version='0.21.1',
+    version='0.22.0',
     author="Bernhard Arnold",
     author_email="bernhard.arnold@oeaw.ac.at",
     packages=find_packages(exclude=['tests']),
