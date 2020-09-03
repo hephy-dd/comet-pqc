ab790764effe915d78fd0e4a2136fb6bfbe610a7
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-09-03 16:40:39 +0200

    comet 0.11.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='comet-pqc',
-    version='0.23.2',
+    version='0.23.3',
     author="Bernhard Arnold",
     author_email="bernhard.arnold@oeaw.ac.at",
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'comet @ https://github.com/hephy-dd/comet/archive/0.11.0.zip#egg=comet-0.11.0',
+        'comet @ https://github.com/hephy-dd/comet/archive/0.11.1.zip#egg=comet-0.11.1',
         'qutie>=1.5.2',
         'pyyaml',
         'jsonschema'
