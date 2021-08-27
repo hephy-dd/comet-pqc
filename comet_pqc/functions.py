6fe27e79b9652af0e8ad7b83dccf03582a8f9e16
Bernhard Arnold <bernhard.arnold@burgried.at>
2021-08-27 15:14:10 +0200

    fixes #157

--- comet_pqc/functions.py
+++ comet_pqc/functions.py
@@ -1,14 +1,23 @@
+"""Functions module."""
+
+from decimal import Context, Decimal
 from typing import Generator
 
 __all__ = ['LinearRange']
 
+ctx: Context = Context(prec=4)
+
+
 class LinearRange:
     """Linear range function generator class.
     Range is bound to [begin, end].
+
     >>> list(LinearRange(0, 10, 2.5)) # positive ramp
     [0.0, 2.5, 5.0, 7.5, 10.0]
+
     >>> list(LinearRange(10, 0, -2.5)) # negative ramp
     [10.0, 7.5, 5.0, 2.5, 0.0]
+
     >>> list(LinearRange(0, 4, -1)) # auto corrected step
     [0.0, 1.0, 2.0, 3.0, 4.0]
     """
@@ -20,23 +29,45 @@ class LinearRange:
     )
 
     def __init__(self, begin: float, end: float, step: float):
-        self.begin = begin
-        self.end = end
-        self.step = step
+        self.begin: float = begin
+        self.end: float = end
+        self.step: float = step
 
     @property
-    def distance(self) -> int:
-        return abs(self.end - self.begin)
+    def distance(self) -> float:
+        """Return distance of linear range.
+
+        >>> LinearRange(-2.5, 2.5, 0.5).distance
+        5.0
+        """
+        begin: Decimal = ctx.create_decimal(self.begin)
+        end: Decimal = ctx.create_decimal(self.end)
+        return abs(float(end - begin))
 
     def __len__(self) -> int:
-        if self.step:
-            return int(abs(round(self.distance / self.step)))
+        step: Decimal = ctx.create_decimal(self.step)
+        distance: Decimal = ctx.create_decimal(self.distance)
+        if step:
+            return int(abs(round(distance / step)))
         return 0
 
-    def __iter__(self) -> Generator:
-        begin, end, step = self.begin, self.end, self.step
-        step = -abs(step) if end < begin else abs(step)
-        count = len(self)
+    def __iter__(self) -> Generator[float, None, None]:
+        begin: Decimal = ctx.create_decimal(self.begin)
+        end: Decimal = ctx.create_decimal(self.end)
+        step: Decimal = ctx.create_decimal(self.step)
+        ascending: bool = begin < end
+        step = abs(step) if ascending else -abs(step)
+        count: int = len(self)
         if count:
-            for i in range(len(self) + 1):
-                yield begin + (i * step)
+            value: Decimal = ctx.create_decimal('NaN')
+            for i in range(count + 1):
+                value = begin + (i * step)
+                # Mangle value not to exceed valid range.
+                if ascending:
+                    value = min(value, end)
+                else:
+                    value = max(value, end)
+                yield float(value)
+            # Yield end if range is incomplete (last odd step).
+            if value != end:
+                yield float(end)
